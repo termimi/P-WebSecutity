817203116ad87856d4b5c1b73480072e6eaaec3a
termimi <123395118+termimi@users.noreply.github.com>
2024-05-03 16:35:03 +0200

    fin de journée

--- Site/backend/API/app/routes/auth.mjs
+++ Site/backend/API/app/routes/auth.mjs
@@ -3,19 +3,68 @@ import express from "express";
 import { databaseConnection } from "../db/dbConnexion.mjs";
 const router = express();
 import crypto from 'crypto';
+import jwt from "jsonwebtoken";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from "url";
 
 
+const getPrivateKeyFromFolder =() =>{
+    // obtient le dossier ou se trouve l'app
+    const _fileName = fileURLToPath(import.meta.url);
+    const _dirname = path.dirname(_fileName);
+    // obtient le chemin du dossier
+    const folderPath = path.join(_dirname, '../keys');
+    // obtient le chemin du fichier
+    const filePath = path.join(folderPath,"/privkey.pem");
+    // lis le fichier
+    const privateKey = fs.readFileSync(filePath);
+    return privateKey;
+}
+
+const getPublicKeyFromFolder =() =>{
+    // obtient le dossier ou se trouve l'app
+    const _fileName = fileURLToPath(import.meta.url);
+    const _dirname = path.dirname(_fileName);
+    // obtient le chemin du dossier
+    const folderPath = path.join(_dirname, '../keys');
+    // obtient le chemin du fichier
+    const filePath = path.join(folderPath,"/pubkey.pem");
+    // lis le fichier
+    const publicKey = fs.readFileSync(filePath);
+    return publicKey;
+}
 router.post('/', databaseConnection, async (req,res) =>{
     // prend les infos du body
     let { username, password } = req.body;
-    // hashage du mot de passe pour le déchifrage
-    password = crypto.createHash('sha256').update(password).digest('hex');
-    // vérifie si l'utilisateur existe
+    // récupère la clef privée
+    const privateKey = getPrivateKeyFromFolder();
+    //recuperation du sel de l'utilisateur
+    const getSaltString = 'SELECT useSalt FROM t_users WHERE useNickName = ?'
+    try{
+        // obtention du sel de l'utilisateur
+        const [UserSalt] = await req.connectionToDB.execute(getSaltString, [username]);
+        if(UserSalt[0].useSalt === undefined || UserSalt[0].useSalt === null){
+            res.status(401).json({message:"L'utilisateur n'est pas enregistré"});
+        }
+        let salt = UserSalt[0].useSalt;
+        // hashage du mot de passe pour le déchifrage
+        password = crypto.createHmac('sha256',salt).update(password).digest('hex');
+    }
+    catch(error){
+        console.error("Impossible de recuperer le sel",error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+    
+    // vérifie si les informations de l'utilisateur sont correct
     const authenticationString = 'SELECT * FROM t_users WHERE useNickName = ? AND usePassWord = ?'
     try{
         const [dbResponse] = await req.connectionToDB.execute(authenticationString, [username, password]);
         if(dbResponse.length > 0){
-            res.send(`Utilisateur: ${username} connecté`);
+            // todo ajouter admin dans le payload
+            //Token
+            const token = jwt.sign({userName: dbResponse[0].useNickName, admin: dbResponse[0].useAdmin },privateKey,{ algorithm: 'RS256' },{ expiresIn: 60 * 60 });
+            res.status(200).json({message:"authentification réuissie" ,token: token});
         }
         else{
             console.log(`Erreur lors de la connexion`);
--- Site/backend/API/app/routes/createUser.mjs
+++ Site/backend/API/app/routes/createUser.mjs
@@ -1,16 +1,33 @@
-import {dbConnection} from "../db/dbConnexion.mjs";
 import express from "express";
 import { databaseConnection } from "../db/dbConnexion.mjs";
 import crypto from 'crypto';
 const router = express();
+
+const getSalt =(lengthOfSalt) =>{
+    return crypto.randomBytes(Math.ceil(lengthOfSalt / 2))
+        .toString('hex')
+        .slice(0, lengthOfSalt);
+}
+const getLengthOfSalt =() =>{
+    return Math.floor(Math.random() * 25);
+}
 router.post('/',databaseConnection,async(req,res) =>{
-    console.log(typeof crypto);
-    let { username, password } = req.body;
-    password = crypto.createHash('sha256').update(password).digest('hex');
+    // récupère les infos du body
+    let { username, password,admin } = req.body;
+    const lengthOfSalt = getLengthOfSalt();
+    const salt = getSalt(lengthOfSalt);
+    // création d'un utilisateur non admin dans le cas ou le champs n'est pas définis
+    if(admin === undefined){
+        admin = 0;
+    }
+    if(admin > 1){
+        res.status(500).json({ error: "La valeur de admin ne peut pas dépasser 1" });
+    }
+    password = crypto.createHmac('sha256',salt).update(password).digest('hex');
     // vérifie si l'utilisateur existe
-    const authenticationString = 'INSERT INTO t_users (useNickName, usePassWord) VALUES (?,?);'
+    const authenticationString = 'INSERT INTO t_users (useNickName, usePassWord,useAdmin,useSalt) VALUES (?,?,?,?);'
     try{
-        const [dbResponse] = await req.connectionToDB.execute(authenticationString, [username, password]);
+        const [dbResponse] = await req.connectionToDB.execute(authenticationString, [username, password,admin,salt]);
         res.send(`Nouvelle utilisateur: ${username} créée`);
     }
     catch(error){
